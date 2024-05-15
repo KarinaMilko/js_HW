20cbcea08663c5d880ddd46b4a6fc8618cbabae9
Karina <milkokarina13@gmail.com>
2024-05-15 19:58:42 +0300

    Add while do for

--- js_HW/assets/js/loops.js
+++ js_HW/assets/js/loops.js
@@ -1,20 +1,93 @@
-// let currentPage = 15;
-// const page = 20;
+// Вивести числа від 25 до 0 (порядок зменшення)
+// 1
 
-// while (currentPage <= page) {
-//   console.log(currentPage++);
+// let startNumber = 25;
+// const endNumber = 0;
+
+// while (startNumber >= endNumber) {
+//   console.log(startNumber--);
 // }
-// let currentPage = Number(prompt("Input start page"));
-// const endPage = Number(prompt("Input end page"));
 
-// while (currentPage <= endPage) {
-//   if (currentPage % 2 === 0) {
-//     console.log(currentPage);
+// 2
+
+// let startNumber = 25;
+// const endNumber = 0;
+
+// do {
+//   console.log(startNumber--);
+// } while (startNumber >= endNumber);
+
+// 3
+
+// const endNumber = 0;
+// for (let startNumber = 25; startNumber >= endNumber; startNumber--) {
+//   console.log(startNumber);
+// }
+
+// Вивести числа від 10 до 50, які кратні 5
+// 1
+
+// let startNumber = 10;
+// const endNumber = 50;
+
+// while (startNumber <= endNumber) {
+//   if (startNumber % 5 === 0) {
+//     console.log(startNumber);
 //   }
-//   currentPage += 1;
+//   startNumber++;
 // }
-let inputNumber = Number(prompt("Input number"));
-while (!Number.isFinite(inputNumber)) {
-  inputNumber = Number(prompt("Input number"));
-}
-console.log(`${inputNumber} ** 2 = ${inputNumber ** 2}`);
+
+// 2
+
+// let startNumber = 10;
+// const endNumber = 50;
+
+// do {
+//   if (startNumber % 5 === 0) {
+//     console.log(startNumber);
+//   }
+//   startNumber++;
+// } while (startNumber <= endNumber);
+
+// 3
+
+// const endNumber = 50;
+// for (let startNumber = 10; startNumber <= endNumber; startNumber++) {
+//   if (startNumber % 5 === 0) {
+//     console.log(startNumber);
+//   }
+// }
+
+// *Знайти суму чисел в межах від 1 до 100
+// 1
+
+// let startNumber = 1;
+// const endNumber = 100;
+// let sum = 0;
+
+// while (startNumber <= endNumber) {
+//   sum = startNumber + sum;
+//   startNumber++;
+// }
+// console.log(sum);
+
+// 2
+
+// let startNumber = 1;
+// const endNumber = 100;
+// let sum = 0;
+
+// do {
+//   sum = startNumber + sum;
+//   startNumber++;
+// } while (startNumber <= endNumber);
+// console.log(sum);
+
+// 3
+// const endNumber = 100;
+// let sum = 0;
+
+// for (let startNumber = 1; startNumber <= endNumber; startNumber++) {
+//   sum = sum + startNumber;
+// }
+// console.log(sum);
